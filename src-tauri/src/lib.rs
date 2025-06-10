d52725a705dead040f2734701190d9288cf01e1a
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2025-06-11 01:04:58 +0600

    add angular-material, init monitor component & add winapi crate for getting system information

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,25 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::mem;
+use winapi::um::sysinfoapi::{GetSystemInfo, SYSTEM_INFO};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn show_message() -> String {
+    unsafe {
+        let mut sys_info: SYSTEM_INFO = mem::zeroed();
+        GetSystemInfo(&mut sys_info);
+
+        format!(
+            "Processors: {}\nProcessor Type: {}\nPage Size: {}\nProcessor Architecture: {}",
+            sys_info.dwNumberOfProcessors,
+            sys_info.dwProcessorType,
+            sys_info.dwPageSize,
+            sys_info.u.s().wProcessorArchitecture
+        )
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -9,6 +27,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![show_message])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () =>
+      import("./views/monitor/monitor.component").then(
+        (m) => m.MonitorComponent
+      ),
+  },
+];
